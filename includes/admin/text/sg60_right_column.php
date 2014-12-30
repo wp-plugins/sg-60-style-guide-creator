11e3780925c8c9f1e90e2fd50c8ca8441115455b
ARC CTRL <ARC CTRL@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-30 19:33:17 +0000

    admin docs update
    git-svn-id: https://plugins.svn.wordpress.org/sg-60-style-guide-creator/trunk@1057067 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/admin/text/sg60_right_column.php
+++ includes/admin/text/sg60_right_column.php
@@ -1,10 +1,14 @@
 <?php
 
-echo '<table width="100%" cellpadding="10"><tr><td width="50% valign="top">';
+echo '<table width="100%" cellpadding="10"><tr><td valign="top">';
 
 echo '<h2>SG-90 Style Guide Creator</h2>';
 echo '<p>ARC(CTRL) is pleased to announce <strong>SG-90</strong> the followup to our SG-60 Style Guide Creator</p>';
 echo '<p>SG-90 was built from the ground up to be a unique way to not just create Style Guides, but gives you the flexibility to create presentations to <strong>wow</strong> your clients</p>';
+
+echo '<h2>Level Up your WordPress Design Business</h2>';
+echo '<iframe width="100%" height="500" src="//www.youtube.com/embed/j43_EL3AsU4" frameborder="0" allowfullscreen></iframe>';
+echo '<p style="text-align:center"><a href="http://arcctrl.com/downloads/sg-90/" target="_blank" style="background:#d2322d;border-radius:10px;color:white;font-size:25px;padding:10px 20px;text-decoration:none;" >Download SG-90</a></p>';
 
 echo '<h2>Communicate Your Vision…</h2>';
 echo '<p>SG-90 allows you full control over which sections (Fonts, Colors, Images, etc.) you load allowing you to create limitless unique style, brand, or other guides.</p>';
@@ -13,13 +17,6 @@ echo '<h2>SG-90 Extensions</h2>';
 echo '<p>SG-90 includes many of the sections you love in SG-60, but offers developers and the community the ability to create their own.<p>';
 echo '<h2>Google Font Extension - FREE</h2>';
 echo '<p>The <a target="_blank" href="http://arcctrl.com/products/sg-90-google-font-extension/">Google Font Extensions</a> which gives you access to use the full Google Font library. The Google Font Extension is <strong>FREE</strong></p>';
-
-echo '<h1>Level Up your WordPress Design Business</h1>';
-echo '<p><a href="http://arcctrl.com/downloads/sg-90/" target="_blank" style="font-size:35px;padding:20px 0;text-decoration:none;">Download SG-90</a></p>';
-
-echo '</td><td valign="top">';
-
-echo '<p><img src="http://arcctrl.com/wp-content/uploads/2014/12/sg90_featureModal-03.gif" width="100%" /></p>';
 
 echo '</td></tr></table>';
 
